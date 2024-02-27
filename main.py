1f6b07d5b0bce495825d41c120cef04a70047b48
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-02-27 20:41:43 +0700

    Add air4thai

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Sub ng")
+from dash import Dash, html, dash_table
+import pandas as pd
+
+
+df = pd.read_csv('air4thai_44t_2024-02-19_2024-02-20.csv')
+
+
+app = Dash(__name__)
+
+
+app.layout = html.Div([
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
