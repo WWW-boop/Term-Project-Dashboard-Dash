6338a53cd50baa13c9203a926f873167aa0aa79a
WWW <119711067+WWW-boop@users.noreply.github.com>
2024-03-09 17:41:41 +0700

    test predict

--- csv/Clean.py
+++ csv/Clean.py
@@ -1,8 +1,8 @@
 import pandas as pd
-df1 = pd.read_csv('csv/pm25.csv')
+df1 = pd.read_csv('pm25.csv')
 df1['PM25'] = df1['PM25'].fillna(df1['PM25'].mean())
 df1['O3'] = df1['O3'].fillna(df1['O3'].mean())
 df1['PM25'] = df1['PM25'].round(1)
 df1['O3'] = df1['O3'].round(1)
 data = df1.drop(columns=['PM10','CO', 'NO2', 'SO2'])
-data.to_csv('csv/pm25_new.csv', index=False)
+data.to_csv('pm25_new11.csv', index=False)
