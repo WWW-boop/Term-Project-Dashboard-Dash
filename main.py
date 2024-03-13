1a2ff262e5fa85a991601134cb55c8c51343174a
Jedex2 <Apirug.pr@gmail.com>
2024-03-13 18:28:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
 import dash_bootstrap_components as dbc
-import numpy as np
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SKETCHY])
 
-df = pd.read_csv('csv/pm25_new.csv')
+# Load data
+df = pd.read_csv('csv/pm25_new2.csv')
+pdi = pd.read_csv('csv/predicted_pm25--5.csv')
+pdw = pd.read_csv('csv/predicted_WD.csv')
 
-pollutants = ['PM25','O3', 'WS','TEMP', 'RH', 'WD']
+# Define pollutants and columns
+pollutants = ['PM25', 'O3', 'WS', 'TEMP', 'RH', 'WD']
 columns = [{'label': col, 'value': col} for col in df.columns if col in pollutants]
 columns.append({'label': 'All Pollutants', 'value': 'all'})
 
+# Layout of the app
 app.layout = html.Div([
+    # Date pickers
     html.Div([
         dcc.Loading(
             dcc.DatePickerSingle(
@@ -42,6 +47,8 @@ app.layout = html.Div([
             type="cube"
         ),
     ], style={'display': 'flex'}),
+
+    # Dropdown for selecting pollutant and chart type
     html.Div([
         dcc.Loading(
             dcc.Dropdown(
@@ -52,10 +59,6 @@ app.layout = html.Div([
             ),
             type="cube"
         ),
-    ]),
-
-    # เพิ่มโค้ดที่ต้องการรวมตรงนี้
-    html.Div([
         html.Label('Select Your Favorite Chart Under:'),
         dcc.Dropdown(
             id='chart-type-dropdown',
@@ -70,18 +73,19 @@ app.layout = html.Div([
         html.Div(id='selected-chart')
     ]),
 
+    # Graphs
     html.Div([
         html.Div([
             dcc.Loading(
-            dcc.Graph(id='air-quality-graph'),
-            type="cube"
-        )
+                dcc.Graph(id='air-quality-graph'),
+                type="cube"
+            )
         ], className="six columns"),
         html.Div([
             dcc.Loading(
-            dcc.Graph(id='example-scatter-plot'),
-            type="cube"
-        )
+                dcc.Graph(id='example-scatter-plot'),
+                type="cube"
+            )
         ], className="six columns"),
         html.Div([
             dcc.Loading(
@@ -96,32 +100,112 @@ app.layout = html.Div([
             type="cube"
         )
     ]),
+    #predict zone
+    html.Div([
+        dcc.Loading(
+            dcc.DatePickerSingle(
+                id='start-date-picker-v2',
+                min_date_allowed=pd.to_datetime('2024-02-28'),
+                max_date_allowed=pd.to_datetime('2024-03-28'),
+                initial_visible_month=pd.to_datetime('2024-02-28'),
+                date=pd.to_datetime('2024-02-28'),
+                display_format='YYYY-MM-DD',
+                className="btn btn-success btn-outline-danger"
+            ),
+            type="cube"
+        ),
+        dcc.Loading(
+            dcc.DatePickerSingle(
+                id='end-date-picker-v2',
+                min_date_allowed=pd.to_datetime('2024-02-29'),
+                max_date_allowed=pd.to_datetime('2024-03-28'),
+                initial_visible_month=pd.to_datetime('2024-03-28'),
+                date=pd.to_datetime('2024-03-28'),
+                display_format='YYYY-MM-DD',
+                className="btn btn-success btn-outline-danger"
+            ),
+            type="cube"
+        ),
 
+    ]),
+    dcc.Graph(id='predict-PM25-graph'),
+    #wd zone
+    html.Div([
+        dcc.Loading(
+            dcc.DatePickerSingle(
+                id='start-date-picker-v3',
+                min_date_allowed=pd.to_datetime('2024-02-28'),
+                max_date_allowed=pd.to_datetime('2024-03-28'),
+                initial_visible_month=pd.to_datetime('2024-02-28'),
+                date=pd.to_datetime('2024-02-28'),
+                display_format='YYYY-MM-DD',
+                className="btn btn-success btn-outline-danger"
+            ),
+            type="cube"
+        ),
+        dcc.Loading(
+            dcc.DatePickerSingle(
+                id='end-date-picker-v3',
+                min_date_allowed=pd.to_datetime('2024-02-29'),
+                max_date_allowed=pd.to_datetime('2024-03-28'),
+                initial_visible_month=pd.to_datetime('2024-03-28'),
+                date=pd.to_datetime('2024-03-28'),
+                display_format='YYYY-MM-DD',
+                className="btn btn-success btn-outline-danger"
+            ),
+            type="cube"
+        ),
+    ]),
+    dcc.Graph(id='predict-WD-graph')
 ])
 
 
+# Callbacks
 @app.callback(
     Output('selected-chart', 'children'),
     [Input('chart-type-dropdown', 'value')]
 )
 def update_selected_chart(chart_type):
     if chart_type == 'none':
-        pass
-    elif chart_type == 'scatter':
+        return html.Div()
+    elif chart_type in ['scatter', 'bar', 'pie']:
         return dcc.Loading(
-                    dcc.Graph(id='example-scatter-plot'),
-                    type="cube"
-                )
-    elif chart_type == 'bar':
-        return dcc.Loading(
-            dcc.Graph(id='bar-chart'),
+            dcc.Graph(id='example-scatter-plot' if chart_type == 'scatter' else
+                                'bar-chart' if chart_type == 'bar' else 'pie-chart'),
             type="cube"
         )
-    elif chart_type == 'pie':
-        return dcc.Loading(
-                dcc.Graph(id='pie-chart'),
-                type="cube"
-            )
+
+
+def update_figure(filtered_df, plot_type, selected_pollutant):
+    if selected_pollutant == 'all':
+        if plot_type == 'scatter':
+            fig = go.Figure()
+            for pollutant in pollutants:
+                fig.add_trace(go.Scatter(
+                    x=filtered_df['DATETIMEDATA'],
+                    y=filtered_df[pollutant],
+                    mode='markers',
+                    name=pollutant
+                ))
+        elif plot_type == 'bar':
+            fig = go.Figure(data=[go.Bar(x=pollutants,
+                                        y=[filtered_df[pollutant].sum() for pollutant in pollutants])])
+        else:  # Pie chart
+            fig = go.Figure(data=[go.Pie(labels=pollutants,
+                                        values=[filtered_df[pollutant].sum() for pollutant in pollutants],
+                                        hole=0.4)])
+    else:
+        if plot_type == 'scatter':
+            fig = px.scatter(filtered_df, x='DATETIMEDATA', y=selected_pollutant,
+                            template="simple_white", title=f'{selected_pollutant} over Time')
+        elif plot_type == 'bar':
+            fig = go.Figure(data=[go.Bar(x=[selected_pollutant], y=[filtered_df[selected_pollutant].sum()])])
+        else:  # Pie chart
+            fig = go.Figure(data=[go.Pie(labels=[selected_pollutant],
+                                        values=[filtered_df[selected_pollutant].sum()], hole=0.4)])
+    fig.update_layout(plot_bgcolor='#E4F9F5', paper_bgcolor='#E4F9F5')  # Background color
+    return fig
+
 
 @app.callback(
     Output('air-quality-graph', 'figure'),
@@ -172,6 +256,7 @@ def update_line_graph(selected_pollutant, start_date, end_date):
 
     return fig
 
+
 @app.callback(
     Output('example-scatter-plot', 'figure'),
     [Input('dropdown', 'value'),
@@ -180,31 +265,18 @@ def update_line_graph(selected_pollutant, start_date, end_date):
 )
 def update_example_scatter_plot(selected_pollutant, start_date, end_date):
     filtered_df = df[(df['DATETIMEDATA'] >= start_date) & (df['DATETIMEDATA'] <= end_date)]
-    if selected_pollutant == 'all':
-        fig = go.Figure()
-        for pollutant in pollutants:
-            fig.add_trace(go.Scatter(
-                x=filtered_df['DATETIMEDATA'],
-                y=filtered_df[pollutant],
-                mode='markers',
-                name=pollutant
-            ))
-        layout = go.Layout(
-            title='Air Quality Scatter Plot',
-            xaxis=dict(title='Date'),
-            yaxis=dict(title='Concentration'),
-            hovermode='closest',
-            template="simple_white",
-            plot_bgcolor='#E4F9F5',  # สีพื้นหลังกราฟ
-            paper_bgcolor='#E4F9F5',  # สีพื้นกระดาน
-        )
-        fig.update_layout(layout)
-    else:
-        fig = px.scatter(filtered_df, x='DATETIMEDATA', y=selected_pollutant,
-                        template="simple_white", title=f'{selected_pollutant} over Time')
-        fig.update_layout(plot_bgcolor='#E4F9F5', paper_bgcolor='#E4F9F5')  # สีพื้นหลังกราฟและพื้นกระดาน
-    return fig
+    return update_figure(filtered_df, 'scatter', selected_pollutant)
 
+
+@app.callback(
+    Output('bar-chart', 'figure'),
+    [Input('dropdown', 'value'),
+    Input('start-date-picker', 'date'),
+    Input('end-date-picker', 'date')]
+)
+def update_bar_chart(selected_pollutant, start_date, end_date):
+    filtered_df = df[(df['DATETIMEDATA'] >= start_date) & (df['DATETIMEDATA'] <= end_date)]
+    return update_figure(filtered_df, 'bar', selected_pollutant)
 
 
 @app.callback(
@@ -215,60 +287,74 @@ def update_example_scatter_plot(selected_pollutant, start_date, end_date):
 )
 def update_pie_chart(selected_pollutant, start_date, end_date):
     filtered_df = df[(df['DATETIMEDATA'] >= start_date) & (df['DATETIMEDATA'] <= end_date)]
-    if selected_pollutant == 'all':
-        fig = go.Figure(data=[go.Pie(labels=pollutants, values=[filtered_df[pollutant].sum() for pollutant in pollutants], hole=0.4)])
-        layout = go.Layout(
-            title='Total Concentration by Pollutant',
-            template="simple_white",
-            plot_bgcolor='#E4F9F5',  # สีพื้นหลังกราฟ
-            paper_bgcolor='#E4F9F5',
-        )
-        fig.update_layout(layout)
-    else:
-        fig = go.Figure(data=[go.Pie(labels=[selected_pollutant], values=[filtered_df[selected_pollutant].sum()], hole=0.4)])
-        layout = go.Layout(
-            title=f'Total Concentration of {selected_pollutant}',
-            template="simple_white",
-            plot_bgcolor='#E4F9F5',  # สีพื้นหลังกราฟ
-            paper_bgcolor='#E4F9F5',
-        )
-        fig.update_layout(layout)
-    return fig
+    return update_figure(filtered_df, 'pie', selected_pollutant)
 
+#predict zone
 @app.callback(
-    Output('bar-chart', 'figure'),
-    [Input('dropdown', 'value'),
-    Input('start-date-picker', 'date'),
-    Input('end-date-picker', 'date')]
+    Output('predict-PM25-graph', 'figure'),
+    [Input('start-date-picker-v2', 'date'),
+    Input('end-date-picker-v2', 'date')]
 )
-def update_bar_chart(selected_pollutant, start_date, end_date):
-    filtered_df = df[(df['DATETIMEDATA'] >= start_date) & (df['DATETIMEDATA'] <= end_date)]
-    if selected_pollutant == 'all':
-        fig = go.Figure(data=[go.Bar(x=pollutants, y=[filtered_df[pollutant].sum() for pollutant in pollutants])])
-        layout = go.Layout(
-            title='Total Concentration by Pollutant (Bar Chart)',
-            xaxis=dict(title='Pollutant'),
-            yaxis=dict(title='Total Concentration'),
-            template="simple_white",
-            plot_bgcolor='#E4F9F5',  # สีพื้นหลังกราฟ
-            paper_bgcolor='#E4F9F5',
-        )
-        fig.update_layout(layout)
-    else:
-        fig = go.Figure(data=[go.Bar(x=[selected_pollutant], y=[filtered_df[selected_pollutant].sum()])])
-        layout = go.Layout(
-            title=f'Total Concentration of {selected_pollutant} (Bar Chart)',
-            xaxis=dict(title='Pollutant'),
-            yaxis=dict(title='Total Concentration'),
-            template="simple_white",
-            plot_bgcolor='#E4F9F5',  # สีพื้นหลังกราฟ
-            paper_bgcolor='#E4F9F5',
-        )
-        fig.update_layout(layout)
+def update_predict_PM25(start_date, end_date):
+    filtered_pdi = pdi[(pdi['DATETIMEDATA'] >= start_date) & (pdi['DATETIMEDATA'] <= end_date)]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=filtered_pdi['DATETIMEDATA'], y=filtered_pdi['prediction_label'], mode='lines+markers'))
+    fig.update_layout(
+        title='Prediction PM25',
+        xaxis_title='Date',
+        yaxis_title='Prediction',
+        template="plotly_white",
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label="1d", step="day", stepmode="backward"),
+                    dict(count=7, label="1w", step="day", stepmode="backward"),
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(visible=True),
+            type="date"
+        ),
+
+        xaxis_range=[start_date, end_date]
+    )
     return fig
+    #wd zone
+@app.callback(
+    Output('predict-WD-graph', 'figure'),
+    [Input('start-date-picker-v3', 'date'),
+    Input('end-date-picker-v3', 'date')]
+)
+def update_predict_wd(start_date, end_date):
+    filtered_pdw = pdw[(pdw['DATETIMEDATA'] >= start_date) & (pdw['DATETIMEDATA'] <= end_date)]
 
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=filtered_pdw['DATETIMEDATA'], y=filtered_pdw['prediction_label'], mode='lines+markers'))
+    fig.update_layout(
+        title='Prediction WD',
+        xaxis_title='Date',
+        yaxis_title='Prediction',
+        template="plotly_white",
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label="1d", step="day", stepmode="backward"),
+                    dict(count=7, label="1w", step="day", stepmode="backward"),
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(visible=True),
+            type="date"
+        ),
 
-
+        xaxis_range=[start_date, end_date]
+    )
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
